ac0790782686a08186522f1e406b509d66b21530
Yong <y.zhou@fz-juelich.de>
2020-03-09 19:00:26 +0100

    [Add] default public/private key pairs

---
+++ src/TunnelAgent/private_key.h
@@ -0,0 +1,15 @@
+/*
+ * private_key.h
+ *
+ *  Created on: 2017年9月27日
+ *      Author: hongxu
+ */
+
+#ifndef SRC_TA_PRIVATE_KEY_H_
+#define SRC_TA_PRIVATE_KEY_H_
+
+#include <string>
+
+extern const std::string g_private_key;
+
+#endif /* SRC_TA_PRIVATE_KEY_H_ */
---
+++ src/TunnelAgent/public_key.h
@@ -0,0 +1,15 @@
+/*
+ * public_key.h
+ *
+ *  Created on: 2017年9月27日
+ *      Author: hongxu
+ */
+
+#ifndef SRC_TA_PUBLIC_KEY_H_
+#define SRC_TA_PUBLIC_KEY_H_
+
+#include <string>
+
+extern const std::string g_public_key;
+
+#endif /* SRC_TA_PUBLIC_KEY_H_ */
